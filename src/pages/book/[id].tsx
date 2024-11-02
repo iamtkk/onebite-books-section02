94a8a84708760b3cdd5a2e338a63f4345da1c7eb
김태관 <iamtk@neowiz.com>
2024-11-02 18:44:22 +0900

    ISR

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,29 +1,31 @@
-import styles from './[id].module.css';
-import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
-import fetchOneBook from '@/lib/fetch-one-book';
-import { useRouter } from 'next/router';
+import styles from "./[id].module.css";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import fetchOneBook from "@/lib/fetch-one-book";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 export const getStaticPaths = async () => {
   return {
     paths: [
-      { params: { id: '1' } }, 
-      { params: { id: '2' } }, 
-      { params: { id: '3' } }
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
     ],
     fallback: true,
+    // false : 404 Not Found
+    // blocking : SSR 방식
+    // true : SSR 방식 + 데이터가 없는 폴백 상태의 페이지부터 반환
   };
 };
 
-export const getStaticProps = async (
-  context: GetStaticPropsContext
-) => {
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
 
   if (!book) {
     return {
       notFound: true,
-    }
+    };
   }
 
   return {
@@ -31,31 +33,50 @@ export const getStaticProps = async (
   };
 };
 
-const Book = ({
-  book,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Book = ({ book }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const router = useRouter();
 
   if (router.isFallback) {
-    return <div>Loading...</div>;
+    return (
+      <>
+        <Head>
+          <title>한입북스</title>
+          <meta name="og:image" content="/thumbnail.png" />
+          <meta name="og:title" content="한입북스" />
+          <meta
+            name="og:description"
+            content="한입 북스에 등록된 도서들을 만나보세요"
+          />
+        </Head>
+        <div>Loading...</div>
+      </>
+    );
   }
 
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
   return (
-    <div className={styles.container}>
-      <div
-        className={styles.cover_img_container}
-        style={{ backgroundImage: `url('${coverImgUrl}')` }}
-      >
-        <img src={coverImgUrl} alt='coverImg' />
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="og:image" content={coverImgUrl} />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={description} />
+      </Head>
+      <div className={styles.container}>
+        <div
+          className={styles.cover_img_container}
+          style={{ backgroundImage: `url('${coverImgUrl}')` }}
+        >
+          <img src={coverImgUrl} alt="coverImg" />
+        </div>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.subTitle}>{subTitle}</div>
+        <div className={styles.author}>
+          {author} | {publisher}
+        </div>
+        <div className={styles.description}>{description}</div>
       </div>
-      <div className={styles.title}>{title}</div>
-      <div className={styles.subTitle}>{subTitle}</div>
-      <div className={styles.author}>
-        {author} | {publisher}
-      </div>
-      <div className={styles.description}>{description}</div>
-    </div>
+    </>
   );
 };
 export default Book;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
-import SearchableLayout from '@/components/searchable-layout';
-import styles from './index.module.css';
-import BookItem from '@/components/book-item';
-import { InferGetStaticPropsType } from 'next';
-import fetchBooks from '@/lib/fetch-books';
-import fetchRandomBooks from '@/lib/fetch-random-books';
+import SearchableLayout from "@/components/searchable-layout";
+import styles from "./index.module.css";
+import BookItem from "@/components/book-item";
+import { InferGetStaticPropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
-  console.log('인덱스 페이지');
-
   const [allBooks, randomBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -22,20 +21,31 @@ const Home = ({
   randomBooks,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <div className={styles.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {randomBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {allBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta name="og:image" content="/thumbnail.png" />
+        <meta name="og:title" content="한입북스" />
+        <meta
+          name="og:description"
+          content="한입 북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
+      <div className={styles.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {randomBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+      </div>
+    </>
   );
 };
 export default Home;
