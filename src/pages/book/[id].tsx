40fe9f91aca5841fcd212234ee3b29fed4f58af6
김태관 <iamtk@neowiz.com>
2024-11-02 10:23:39 +0900

    SSG

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,12 +1,31 @@
 import styles from './[id].module.css';
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
 import fetchOneBook from '@/lib/fetch-one-book';
+import { useRouter } from 'next/router';
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { id: '1' } }, 
+      { params: { id: '2' } }, 
+      { params: { id: '3' } }
+    ],
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async (
+  context: GetStaticPropsContext
 ) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
+
+  if (!book) {
+    return {
+      notFound: true,
+    }
+  }
+
   return {
     props: { book },
   };
@@ -14,10 +33,13 @@ export const getServerSideProps = async (
 
 const Book = ({
   book,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  if (!book) {
-    return <div>Book not found</div>;
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
   }
+
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
   return (
     <div className={styles.container}>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,22 +1,32 @@
 import SearchableLayout from '@/components/searchable-layout';
 import BookItem from '@/components/book-item';
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
+import { useEffect, useState } from 'react';
+import { BookData } from '@/types';
+import { useRouter } from 'next/router';
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const q = context.query.q;
-  const books = await fetchBooks(q as string);
+// export const getStaticProps = async (
+//   context: GetStaticPropsContext
+// ) => {
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
 
-  return {
-    props: { books },
-  };
-};
+//   return {
+//     props: { books },
+//   };
+// };
 
-const Search = ({
-  books,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Search = () => {
+  const [books, setBooks] = useState<BookData[]>([]);
+  const router = useRouter();
+  const q = router.query.q;
+
+  useEffect(() => {
+    if (q) {
+      fetchBooks(q as string).then(setBooks);
+    }
+  }, [q]);
+
   return (
     <div>
       {books.map((book) => (
